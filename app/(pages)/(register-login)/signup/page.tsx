fd6c28ec18e5efa870c06e9258a51831ffa4bc7b
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-04-13 22:29:08 +0530

    Added: (UI) Signup and login page UI

--- app/(pages)/(register-login)/signup/page.tsx
+++ app/(pages)/(register-login)/signup/page.tsx
@@ -1,13 +1,20 @@
 "use client"
 import { useState } from 'react';
 
+
 interface FormData {
+  username: string;
+  fullName: string;
+  bio: string;
   email: string;
   password: string;
 }
 
 export default function Page() {
   const [formData, setFormData] = useState<FormData>({
+    username: '',
+    fullName: '',
+    bio: '',
     email: '',
     password: '',
   });
@@ -22,18 +29,85 @@ export default function Page() {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    console.log(formData);
   };
+
+  // const register = async() => {
+  //   const registerRefrence = await fetch("/api/users/signup", 
+  //     {
+  //       method: "POST",
+  //       headers: {
+  //         "content-type": "application/json"
+  //       },
+  //       body: JSON.stringify(formData) 
+  //     }); 
+
+  //     console.log(registerRefrence); 
+  // }; 
+
 
   return (
     <div className="min-h-screen bg-black flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-orange-200">Login to your account</h2>
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-orange-200">Sign up for an account</h2>
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md ">
         <div className="bg-black py-8 px-4 shadow-lg sm:rounded-lg sm:px-10 bg-opacity-50 backdrop-blur-lg border border-[#ffffff70]">
           <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="username" className="block text-sm font-medium text-gray-300">
+                Username
+              </label>
+              <div className="mt-1">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="username"
+                  required
+                  value={formData.username}
+                  onChange={handleChange}
+                  className="appearance-none block w-full px-3 py-2 bg-[#ffffff5d] border border-gray-600 rounded-md shadow-sm placeholder-gray-500 text-black focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="fullName" className="block text-sm font-medium text-gray-300">
+                Full Name
+              </label>
+              <div className="mt-1">
+                <input
+                  id="fullName"
+                  name="fullName"
+                  type="text"
+                  autoComplete="fullName"
+                  required
+                  value={formData.fullName}
+                  onChange={handleChange}
+                  className="appearance-none block w-full px-3 py-2 bg-[#ffffff5d] border border-gray-600 rounded-md shadow-sm placeholder-gray-500 text-black focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="bio" className="block text-sm font-medium text-gray-300">
+                Bio
+              </label>
+              <div className="mt-1">
+                <input
+                  id="bio"
+                  name="bio"
+                  type="text"
+                  autoComplete="bio"
+                  required
+                  value={formData.bio}
+                  onChange={handleChange}
+                  className="appearance-none block w-full px-3 py-2 bg-[#ffffff5d] border border-gray-600 rounded-md shadow-sm placeholder-gray-500 text-black focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm"
+                />
+              </div>
+            </div>
+
             <div>
               <label htmlFor="email" className="block text-sm font-medium text-gray-300">
                 Email address
@@ -84,4 +158,3 @@ export default function Page() {
     </div>
   );
 }
-
